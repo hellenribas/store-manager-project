a832e5cf90ce30060b839f3a46c825f39b270ca9
hellenribas <tchwtrabalho@gmail.com>
2022-06-29 19:37:41 -0300

    fix: error avaliator

--- tests/unit/models/productsTest.test.js
+++ tests/unit/models/productsTest.test.js
@@ -1,12 +1,44 @@
-// const {
-//   expect
-// } = require('chai');
+const {
+  expect
+} = require('chai');
+const connection = require('../../../helpers/connection');
 
-// describe('testing the functions of getting products in the database', () => {
-//   describe('success case', () => {
+const sinon = require('sinon');
 
-//   })
-//   describe('case of no success', () => {
+const {
+  getProductsAllModel
+} = require('../../../models/productsModel');
 
-//   })
-// })
+describe('testing the functions of getting products in the database', () => {
+  describe('success case', () => {
+
+  before(async () => {
+    const products = [{
+      id: 1,
+      name: 'exemplo de produto'
+    }];
+    sinon.stub(connection, 'execute').returns(products);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+  
+  it('return a object', async () => {
+    const response = await getProductsAllModel();
+
+    expect(response).to.be.a('object')
+  });
+  
+  it('my object has name and id property', async () => {
+    const response = await getProductsAllModel();
+
+    expect(response).to.have.a.property('name');
+    expect(response).to.have.a.property('id');
+  });
+  
+  })
+  describe('case of no success', () => {
+
+  })
+})
