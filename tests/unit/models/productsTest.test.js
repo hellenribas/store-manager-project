fe23541be8c21912c5a07a19c435bc4c63c03ad2
hellenribas <tchwtrabalho@gmail.com>
2022-06-29 18:58:51 -0300

    fix: error connection database

---
+++ tests/unit/models/productsTest.test.js
@@ -0,0 +1,12 @@
+// const {
+//   expect
+// } = require('chai');
+
+// describe('testing the functions of getting products in the database', () => {
+//   describe('success case', () => {
+
+//   })
+//   describe('case of no success', () => {
+
+//   })
+// })
