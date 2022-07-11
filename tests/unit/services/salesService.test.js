2a2d2a968d6e1ebebff0716a37e18743f22a49a2
hellenribas <tchwtrabalho@gmail.com>
2022-07-11 18:55:43 -0300

    fix: excluding comment

--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -11,41 +11,6 @@ const salesModel = require('../../../models/salesModel');
 const middlewares = require('../../../services/middlewareValidate');
 
 describe('testing productValidate', () => {
-  // describe('sucess insert case', () => {
-  //   const resultArray = [];
-  //   const returnId = {
-  //     id: 1,
-  //   };
-  //   const product = [{
-  //     productId: 1,
-  //     quantity: 1
-  //   }];
-    
-  //   const salesId = {
-  //     saleId: 1,
-  //   };
-
-  //   before(() => {
-  //     sinon.stub(middlewares, 'productValidate').resolves(resultArray);
-  //     sinon.stub(middlewares, 'validateSales').resolves(resultArray);
-  //     sinon.stub(salesModel, 'insertSalesModel').resolves(returnId);
-  //     sinon.stub(salesModel, 'salesDate').resolves(salesId);
-  //   });
-
-  //   after(() => {
-  //     sinon.restore();
-  //   });
-  
-  //   it('return a object', async () => {
-  //     const result = await salesService.insertSalesService(product);
-  //     console.log(result);
-  //     expect(result).to.be.an('object');
-  //   });
-  //   it('return a object with property id', async () => {
-  //     const result = await salesService.insertSalesService(product);
-  //     expect(result).to.have.property('id');
-  //   });
-  // });
    describe('no sucess insert case', () => {
      const resultErrorValidate = {
        error: {
